18d5afd35aee5af9986b16260de7ba78e1888001
xxxstudio <liu.aim@qq.com>
2022-04-10 17:19:00 +0800

    feat

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', '47.115.60.250'),
   port: env.int('PORT', 1337),
+  url: 'http://47.115.60.250:1337',
   app: {
     keys: env.array('APP_KEYS'),
   },
